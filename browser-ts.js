6366e963e711195167cdc4de24ace0331f4140c7
anbcodes <anbcodes@protonmail.com>
2021-12-24 14:17:51 -0500

    added support for type="application/typescript" scripts

--- browser-ts.js
+++ browser-ts.js
@@ -1,3 +1,7 @@
+const newScript = document.createElement("script");
+newScript.src = "https://unpkg.com/typescript@4.5.4/lib/typescriptServices.js";
+document.body.appendChild(newScript);
+
 function browserTS(url = "./browser-ts-sw.js") {
   if ("serviceWorker" in navigator) {
     const interceptorLoaded = navigator.serviceWorker.controller != null;
@@ -17,8 +21,45 @@ function browserTS(url = "./browser-ts-sw.js") {
         });
     });
   }
+
+  const compile = (str) => {
+    let replaced = 'console.error("Failed to compile ts module")';
+    try {
+      console.time("Compiling");
+      replaced = ts.transpileModule(str, {
+        compilerOptions: {
+          target: "ES2020",
+          module: "ES2020",
+        },
+      }).outputText;
+      console.timeEnd("Compiling");
+    } catch (e) {
+      console.error(e);
+      replaced = 'throw new Error("Failed to compile ts module: ' + e.message +
+        '")';
+    }
+
+    return replaced;
+  };
+
+  newScript.addEventListener('load', () => {
+    document.querySelectorAll('script[type="application/typescript"]').forEach(
+      (script) => {
+        const js = compile(script.textContent);
+        const newScript = document.createElement("script");
+        const textNode = document.createTextNode(
+          "/* Typescript\n" + script.textContent + "*/\n" + js,
+        );
+        newScript.type = "module";
+        newScript.appendChild(textNode);
+
+        script.after(newScript);
+        script.remove();
+      },
+    );
+  });
 }
 
 async function browserTSUnregister() {
-  (await navigator.serviceWorker.getRegistrations())[0].unregister()
+  (await navigator.serviceWorker.getRegistrations())[0].unregister();
 }
